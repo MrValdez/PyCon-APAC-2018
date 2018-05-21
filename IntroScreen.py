75cb1fbbfdf2b1b479cd47a197508b54357f8357
MrValdez <shunyvaldez@gmail.com>
2018-05-22 00:15:03 +0800

    added text and scrolling in intro screen

--- IntroScreen.py
+++ IntroScreen.py
@@ -8,18 +8,97 @@ class IntroScreen(Screen):
     def __init__(self, WindowSize):
         self.texts = [
             Text(WindowSize[0] / 2, flip_y(WindowSize, 110), "The source code for this presentation can be found at\nhttps://github.com/MrValdez/PyCon-APAC-2018"),
+            Text(WindowSize[0] / 2, 0, """Anyone can make a game...
+
+It just take a lot of effort.
+
+In this talk, MrValdez will be introducing the basics of Game Programming.
+
+By the end, we would have enough to make a simple game.
+
+
+
+
+But he can't do it alone.
+
+He'll need your help.
+
+
+
+
+3 volunteers from the audience to help:
+
+
+
+Two artists.    One player.
+
+
+
+This is the one hour story of how PyCon APAC 2018 made a game.
+"""),
         ]
+        self.WindowSize = WindowSize
+
+        artist1 = arcade.Sprite("crono_back.gif")
+        artist1.center_x = 375
+        artist1.center_y = -660
+        
+        artist2 = arcade.Sprite("crono_back.gif")
+        artist2.center_x = 475
+        artist2.center_y = -660
+        
+        tester = arcade.Sprite("crono_back.gif")
+        tester.center_x = 600
+        tester.center_y = -660
+
+        self.sprites = arcade.SpriteList()
+        self.sprites.append(artist1)
+        self.sprites.append(artist2)
+        self.sprites.append(tester)
 
     def load(self):
         arcade.set_background_color(arcade.color.BLACK)
         self.gameState = GameState.RUNNING
+        self.scroll = 0
+        self.start_timer = 7
+        self.scroll_speed = 0.7
+        self.end_timer = 5
+
+        #debug values:
+        #self.start_timer = 0
+        #self.scroll_speed = 3
 
     def update(self, delta_time):
+        if self.start_timer > 0:
+            # wait for timer to clear to start scrolling
+            self.start_timer -= delta_time
+            return
+
+        left = 0
+        right = self.WindowSize[0]
+        bottom = 0
+        top = self.WindowSize[1]
+
+        self.scroll += self.scroll_speed
+        bottom -= self.scroll
+        top -= self.scroll
+
+        if bottom > -1000:
+            arcade.set_viewport(left, right, bottom, top)
+        else:
+            # we reached the end of the scrolling. start a timer to end the intro screen
+            self.end_timer -= delta_time
+            if self.end_timer < 0:
+                return GameState.NEXT_STAGE
+
         return self.gameState
 
     def draw(self):
         for text in self.texts:
             text.draw()
 
+        self.sprites.draw()
+
     def on_key_press(self, key, modifiers):
-        self.gameState = GameState.NEXT_STAGE
+        pass
+#        self.gameState = GameState.NEXT_STAGE
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from TitleScreen import TitleScreen
 from FadeScreen import FadeScreen
 from IntroScreen import IntroScreen
 
-WindowSize = (800, 600)
+WindowSize = (1024, 600)
 
 class Avatar(arcade.Sprite):
     def __init__(self, *args):
@@ -42,29 +42,35 @@ class Engine(arcade.Window):
 
         self.WindowSize = WindowSize
         self.currentStage = self.stages[0]
+        self.currentStage = self.stages[1]
         self.currentStage.load()
         self.fade = FadeScreen(WindowSize)
         self.gameState = GameState.RUNNING
+
+        self.debug = True
 
     def setup(self):
         self.avatar = Avatar("crono_back.gif")
         self.avatar.center_x = WindowSize[0] / 2
         self.avatar.center_y = WindowSize[1] / 2
-#        self.avatars = arcade.SpriteList()
-#        self.avatars.append(self.avatar)
 
     def on_draw(self):
         arcade.start_render()
 
         self.currentStage.draw()
-        self.avatar.draw()
+        
+        if self.debug:
+            self.avatar.draw()
+            print(self.avatar.position)     # used to check coordinate position. useful in placing elements for slides
 
         if self.gameState == GameState.NEXT_STAGE:
             self.fade.draw()
 
     def update(self, delta_time):
         if self.gameState == GameState.RUNNING:
-            self.gameState = self.currentStage.update(delta_time)
+            returned_game_state = self.currentStage.update(delta_time)
+            if returned_game_state in GameState:
+                self.gameState = returned_game_state
 
             if self.gameState == GameState.NEXT_STAGE:
                 # Start transition to fade out 
@@ -78,9 +84,14 @@ class Engine(arcade.Window):
                 self.gameState = GameState.RUNNING
                 new_stage = self.stages.index(self.currentStage) + 1
                 new_stage %= len(self.stages)
-                self.currentStage = self.stages[new_stage]
-                self.currentStage.load()
+                self.change_stage(new_stage)
 
+    def change_stage(self, new_stage_index):
+        self.currentStage = self.stages[new_stage_index]
+        self.currentStage.load()
+
+        arcade.set_viewport(0, WindowSize[0], 0, WindowSize[1])
+    
     def on_key_press(self, key, modifiers):
         if key == arcade.key.ESCAPE:
             self.close()
