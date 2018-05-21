eee56cd02d5ce1756d88092a98e5b09d9b95f12f
MrValdez <shunyvaldez@gmail.com>
2018-05-22 02:45:53 +0800

    minor change in slide text

--- Slide_TOC.py
+++ Slide_TOC.py
@@ -7,21 +7,21 @@ from helper import (flip_y,
 class Slide(SlideTemplate):
     def __init__(self, WindowSize):
         title = "Stage 2"
-        subtitle = "What knowledge are needed for game programming?"
+        subtitle = "What you need to know to program game"
 
         slides = [[],
 
                   [Text(50, flip_y(WindowSize, 70), "1. Game Library"),],
  
                   [Text(50, flip_y(WindowSize, 70), """1. Game Library
-2, Game loop""")],
+2, Game Programming Design Patterns: Game loop""")],
 
                   [Text(50, flip_y(WindowSize, 70), """1. Game Library
-2. Game loop
+2. Game Programming Design Patterns: Game loop
 3. Object Oriented Programming""")],
 
                   [Text(50, flip_y(WindowSize, 70), """1. Game Library
-2. Game loop
+2. Game Programming Design Patterns: Game loop
 3. Object Oriented Programming
 4. Maths""")],
                  ]
