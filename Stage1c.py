a4344cfaa0cf11d0865a7c758d5896574090d117
MrValdez <shunyvaldez@gmail.com>
2018-05-28 16:56:27 +0800

    updated slides

--- Stage1c.py
+++ Stage1c.py
@@ -11,15 +11,14 @@ class Stage1c(SlideTemplate):
         subtitle = "Who is MrValdez"
 
         slides = [
-                  [Text(50, flip_y(WindowSize, 40), """I'm a video game programming hobbyist
-
+                  [Text(50, flip_y(WindowSize, 40), """- I'm a video game programming hobbyist
 - Active board member at Python Philippines
 - Use game programming to teach programming
 - https://mrvaldez.ph
 - https://github.com/mrvaldez
 - @MrValdez
 """),
-                   Image(600, flip_y(WindowSize, 30), "mrvaldez.png", scale=0.6),]
+                   Image(650, flip_y(WindowSize, 30), "mrvaldez.png", scale=0.6),]
                   ]
 
         super().__init__(WindowSize, title, subtitle, slides)
--- main.py
+++ main.py
@@ -45,8 +45,7 @@ class Engine(arcade.Window):
 
         self.WindowSize = WindowSize
         self.currentStage = self.stages[0]
-        self.currentStage = self.stages[-1]      # debugging
-        self.currentStage = self.stages[3]      # debugging
+#        self.currentStage = self.stages[4]      # debugging
         self.currentStage.load()
         self.fade = FadeScreen(WindowSize)
         self.gameState = GameState.RUNNING
